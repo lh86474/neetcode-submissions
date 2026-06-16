b8a6682056df4965c9076b5fc01fb83b2fe13582
lh86474 <142271688+lh86474@users.noreply.github.com>
2026-06-15 20:08:06 -0700

    Add: invert-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-1.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root) {
+        // I will definitely have to start at the root and keep swapping every time
+        // going to the parent of root is difficult because there is no parent parameter
+        // I will traverse in-order
+        // start on the left
+        if (root == null) {
+            return null;
+        }
+        // I didn't think of using a queue: which was smart as I was trying to figure out a 
+        // a way to traverse other subtrees
+        let queue = [];
+        queue.push(root);
+        let currentNode = null;
+        let left = null;
+        let right = null;
+        let holder = null;
+        while (queue.length > 0) {
+            currentNode = queue.shift();
+            if (currentNode.left !== null) {
+                queue.push(currentNode.left);
+            }
+            if (currentNode.right !== null) {
+                queue.push(currentNode.right);
+            }
+            right = currentNode.right;
+            left = currentNode.left;
+            holder = left;
+            currentNode.left = right;
+            currentNode.right = holder;
+        }
+        return root;
+    }
+}
